6abd781963b767d64fec9c32ce7e290c65f6b867
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2025-03-25 22:57:10 +0100

    Add basic split bill

--- WariKan/ContentView.swift
+++ WariKan/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var billAmount = 10
+    @State private var numberOfPeople = 2
+    
+    var amountPerPerson: Int {
+        return (billAmount / numberOfPeople)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("割り勘")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("何円ですか？", value: $billAmount, format: .currency(code: "JPY"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("何人ですか？", selection: $numberOfPeople) {
+                        ForEach(1...100, id: \.self) {
+                            Text("\($0)人")
+                        }
+                    }
+                }
+                Section("一人当たり") {
+                    Text(amountPerPerson, format: .currency(code: "JPY"))
+                }
+            }
+            .navigationTitle("割り勘")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
