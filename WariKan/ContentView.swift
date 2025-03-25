67b32ed95cf2f3264f4cc0767a9ed50e5ae928aa
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2025-03-25 20:50:44 +0100

    Initial commit

---
+++ WariKan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WariKan
+//
+//  Created by Serrano Soria, Juan on 25/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("割り勘")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WariKan/WariKanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WariKanApp.swift
+//  WariKan
+//
+//  Created by Serrano Soria, Juan on 25/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WariKanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
