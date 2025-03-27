85c1cda50c9a878fd3d136c430e44ad2df32aabe
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2025-03-27 21:50:03 +0100

    Implement tip to payer system

--- WariKan/ContentView.swift
+++ WariKan/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var billAmount = 2000
     @State private var numberOfPeople = 2
+    @State private var tipPercentage = 0
+    
+    let tipPercentages = [0, 5, 10, 15, 20]
     
     var amountPerPerson: Int {
         return billAmount / numberOfPeople
@@ -17,6 +20,14 @@ struct ContentView: View {
     
     var extraAmount: Int {
         return billAmount % numberOfPeople
+    }
+    
+    var tipAmountPerPerson: Int {
+        return amountPerPerson * tipPercentage / 100 + (extraAmount > 0 ? extraAmount * tipPercentage / 100 : 0)
+    }
+    
+    var tipReceived: Int {
+        return tipAmountPerPerson * (numberOfPeople - 1)
     }
     
     var body: some View {
@@ -32,12 +43,26 @@ struct ContentView: View {
                         }
                     }
                 }
+                Section("幹事への感謝金") {
+                    Picker("幹事への感謝金", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id:\.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.palette)
+                }
                 Section("一人当たり") {
                     Text(amountPerPerson, format: .currency(code: "JPY"))
                 }
                 if extraAmount > 0 {
                     Section("調整額") {
-                        Text("幹事は \(extraAmount, format: .currency(code: "JPY")) を調整します！")
+                        Text("幹事様が \(extraAmount, format: .currency(code: "JPY")) を調整させていただきます")
+                    }
+                }
+                if (tipPercentage > 0) {
+                    Section("感謝金") {
+                        Text("一人当たりの感謝金額：\(tipAmountPerPerson, format: .currency(code: "JPY"))")
+                        Text("幹事様への総感謝金：\(tipReceived, format: .currency(code: "JPY"))")
                     }
                 }
             }
